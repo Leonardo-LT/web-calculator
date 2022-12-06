4eafa186463874e4e2f3b5daee87373e85021822
Leonardo <leonardolatorre2000@gmail.com>
2022-12-06 20:00:52 +0100

    add numbers to ui

--- script.js
+++ script.js
@@ -17,3 +17,25 @@ const divide = (num1, num2) => {
 const operate = (operator , num1 , num2) =>{
     return operator(num1 , num2)
 }
+
+const screen = document.getElementById("screen")
+const num = document.querySelectorAll(".key.num")
+const operator = document.querySelectorAll("operator")
+const equal = document.querySelectorAll("equal")
+
+let operatorClicked = false
+let firstNum = ""
+let secondNum = ""
+
+for (let i = 0; i < num.length; i++) {
+    const number = num[i];
+    number.addEventListener("click", () => {
+        if (!operatorClicked) {
+            firstNum += number.textContent
+            screen.textContent = firstNum
+        } else {
+            secondNum += number.textContent
+            screen.textContent = secondNum
+        }
+    })
+}
